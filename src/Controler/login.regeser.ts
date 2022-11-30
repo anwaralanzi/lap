15bcc96fa524bcd11d8f7af72799d1f9b38bea2b
anwar <anwaralanzo6@gmail.com>
2022-11-30 18:33:04 +0300

    lap

--- src/Controler/login.regeser.ts
+++ src/Controler/login.regeser.ts
@@ -1,44 +1,69 @@
-import { User } from "@prisma/client";
+import { user } from "@prisma/client";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { prisma } from "../confing/db";
 import * as argon2 from "argon2";
 import { NextFunction, Request, Response } from "express";
+import  jwt = require('jsonwebtoken');
 
 export const login= async (req: Request, res:Response) => {
-  const { username, password } = req.body as User;
-  const user = await prisma.user.findUnique({
+  const { username, password } = req.body as user;
+  const user2 = await prisma.user.findUnique({
     where: { username },
   });
-  if (!user) {
+  if (!user2) {
     return res
       .status(400).json({
          message: "wrong username or password !" });
   }
-  const isValidPassword = await argon2.verify(user.password, password);
+  const isValidPassword = await argon2.verify(user2.password, password);
   if (!isValidPassword) {
     return res.status(400).json({
          message: "wrong username or password !" });
   }
+  const token = jwt.sign({id: user2.id,role:user2.role},process.env.JWT_SECERT as string)
   
-  return res.status(200).json({ message: "Welcom back!" });
-};
+  return res.status(200).json({ 
+    message: "Welcom back!",token})}
 
 export const register = async (req: Request, res: Response) => {
   try {
-    const newUser = req.body as User;
-    const hashPassword = await argon2.hash(newUser.password);
-    newUser.password = hashPassword;
+    const newUser = req.body as user;
+
+    const hashedPassword = await argon2.hash(newUser.password);
+    newUser.password = hashedPassword;
     await prisma.user.create({
       data: newUser,
     });
-    return res.status(200).json({ message: "Welcom!!" });
+    return res.status(201).json({
+      message: 'Welcome ! , user added !',
+    });
   } catch (error) {
     console.log(error);
     const prismaError = error as PrismaClientKnownRequestError;
-    return res.status(400).json({ message: prismaError.message });
+    return res.status(400).json({
+      message: prismaError.message,
+    });
   }
 };
 export const getAllUsers = async (req: Request, res: Response,next:NextFunction) => {
-  const allusers = await prisma.user.findMany();
-  res.status(200).json(allusers);
+  try {
+    const users = await prisma.user.findMany();
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    const prismaError = error as PrismaClientKnownRequestError;
+    return res.status(400).json({
+      message: prismaError.message,
+    });
+  }
+};
+
+export const AdminH = async (req: Request, res: Response) => {
+  return res
+    .status(200)
+    .json({ message: 'Hi admin' + res.locals.user.id });
+};
+
+export const userH = async (req: Request, res: Response) => {
+  return res.status(200).json({ message: 'Hey user' });
 };
--- src/Router/login.regeser.ts
+++ src/Router/login.regeser.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import  {register,
-    login,getAllUsers
+    login,getAllUsers,AdminH,userH
 } from "../Controler/login.regeser";
+import { authorize, protect } from "../middlwehre/protect";
 import validate from "../middlwehre/validate3";
 import { registerSchema, loginSchema } from "../zodschima/zodSchema";
 
 const router = express.Router();
 
-router.post("/", validate(registerSchema), register);
+router.post("/reg", validate(registerSchema), register);
 router.get("/:id", login);
-router.put("/:id", validate(loginSchema), login);
 router.get("/", getAllUsers);
+router.get('/user', protect, authorize('USER', 'ADMIN'), userH);
+router.get('/Admin', protect, authorize('ADMIN'), AdminH);
+
 
 export default router;
